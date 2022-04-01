f41897470f1c4c27f50ee88b20cad31452ffd1f4
BatyshchevKirill <k.batyshchev@innopolis.university>
2022-04-01 16:50:27 +0300

    Update problem2.java

--- src/ru/innopolis/problem2/problem2.java
+++ src/ru/innopolis/problem2/problem2.java
@@ -1,2 +1,36 @@
-// your solution here
-//fix all the errors including package errors
+public class problem2 {
+    public static void main(String[] args) {
+        ChargerStation station = new ChargerStation();
+        iPhone iPhone = new iPhone();
+        station.chargeByLightning(iPhone);
+        Android android = new Android();
+        AndroidAdapter adapter= new AndroidAdapter(android);
+        station.chargeByLightning(adapter);
+    }
+}
+
+class ChargerStation{
+    void chargeByLightning(iPhone iphone ){
+        iphone.chargeI();
+    }
+}
+class iPhone{
+    void chargeI(){
+        System.out.println("The iphone is charged");
+    }
+}
+class Android{
+    void chargeA(){
+        System.out.println("The android is charged");
+    }
+}
+class AndroidAdapter extends iPhone{
+    private Android android;
+    AndroidAdapter ( Android android){
+        this.android= android;
+    }
+    void chargeI (){
+        android.chargeA();
+    }
+}
+
